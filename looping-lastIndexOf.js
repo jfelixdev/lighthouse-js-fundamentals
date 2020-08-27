945f79f5669504d44ccb080ebd1bed6c5ca54702
Jeremy Felix D <me@jeremyfelix.com>
2020-08-27 01:08:00 -0700

    Create looping-lastIndexOf.js

---
+++ looping-lastIndexOf.js
@@ -0,0 +1,13 @@
+const lastIndexOf = function(arr, val){
+  let lastIndex = -1;
+  arr.forEach((value, index, array) => {
+    if(value === val){ lastIndex = index; }
+  });
+  return lastIndex;
+};
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
